b5078e53b0c8945328314d8d771ac0cbaf355e70
BOYANG ZHANG <bzhang523@twhuang-desktop-06>
2025-04-29 19:40:54 -0500

    added reduce

--- reduce/reduce.cu
+++ reduce/reduce.cu
@@ -412,12 +412,12 @@ void gpu_reduction_ver5(const std::vector<int>& h_data) {
 // CUDA reduction version #6: completely unroll for different block size
 template <unsigned int blockSize>
 __device__ void warpReduce1(volatile int* sdata, int tid) {
-  if(blockSize >= 64) sdata[tid] += sdata[tid + 32];
-  if(blockSize >= 32) sdata[tid] += sdata[tid + 16];
-  if(blockSize >= 16) sdata[tid] += sdata[tid + 8];
-  if(blockSize >= 8) sdata[tid] += sdata[tid + 4];
-  if(blockSize >= 4) sdata[tid] += sdata[tid + 2];
-  if(blockSize >= 2) sdata[tid] += sdata[tid + 1];
+  if constexpr (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+  if constexpr (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+  if constexpr (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+  if constexpr (blockSize >= 8) sdata[tid] += sdata[tid + 4];
+  if constexpr (blockSize >= 4) sdata[tid] += sdata[tid + 2];
+  if constexpr (blockSize >= 2) sdata[tid] += sdata[tid + 1];
 }
 
 template <unsigned int blockSize>
@@ -437,19 +437,19 @@ __global__ void reduce6(int *g_idata, int *g_odata, int N) {
 
   // do reduction in shared memory
   // s is stride, saying the distance between the elements one thread handles
-  if(blockSize >= 512) {
+  if constexpr (blockSize >= 512) {
     if (tid < 256) {
       sdata[tid] += sdata[tid + 256];
     }
     __syncthreads();
   }
-  if(blockSize >= 256) {
+  if constexpr (blockSize >= 256) {
     if (tid < 128) {
       sdata[tid] += sdata[tid + 128];
     }
     __syncthreads();
   }
-  if(blockSize >= 128) {
+  if constexpr (blockSize >= 128) {
     if (tid < 64) {
       sdata[tid] += sdata[tid + 64];
     }
@@ -536,19 +536,19 @@ __global__ void reduce6_plus(int *g_idata, int *g_odata, int N) {
 
   // do reduction in shared memory
   // s is stride, saying the distance between the elements one thread handles
-  if(blockSize >= 512) {
+  if constexpr (blockSize >= 512) {
     if (tid < 256) {
       sdata[tid] += sdata[tid + 256];
     }
     __syncthreads();
   }
-  if(blockSize >= 256) {
+  if constexpr (blockSize >= 256) {
     if (tid < 128) {
       sdata[tid] += sdata[tid + 128];
     }
     __syncthreads();
   }
-  if(blockSize >= 128) {
+  if constexpr (blockSize >= 128) {
     if (tid < 64) {
       sdata[tid] += sdata[tid + 64];
     }
